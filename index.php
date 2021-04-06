1785cf19a272c25e32cb66e83f8875d06f024259
unknown <vquivy@gamingcampus.local>
2021-04-06 17:41:55 +0200

    Modification index

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  <title>One on One</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+</head>
+<body>
+    <nav>
+        <li class="logo"><a href="index.php"><img src="images/eleven.png" height="50" alt="OneonOne"></a></li>
+        <li><a href="#description">En savoir plus</a></li>
+        <li><a href="#galerie">Galerie</a></li>
+        <li><a href="#zywoo">ZywOo</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </nav>
+</body>
+</html>
