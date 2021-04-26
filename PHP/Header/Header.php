6eed4f3bd7be1930b8eb068a41feada22ab974c1
esskaaa <nugierbatiste1@gmail.com>
2021-04-26 13:58:39 +0200

    version finale

--- PHP/Header/Header.php
+++ PHP/Header/Header.php
@@ -4,7 +4,7 @@
   <link rel="stylesheet" type="text/css" href="../../materialize/css/styles.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
   <title>One on One</title>
-  <link rel="icon" type="images" href="Eleven.png" />
+  <link rel="icon" type="image" href="../../images/Eleven.png" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
   <script type="text/javascript" src="../../materialize/js/jquery.min.js"></script>
   <script type="text/javascript" src="../../materialize/js/materialize.min.js"></script>
